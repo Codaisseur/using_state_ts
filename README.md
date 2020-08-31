8876300e09dff5fe5a84a9391c715cb64be333f6
Kelley van Evert <hello@kelleyvanevert.nl>
2020-08-31 10:17:27 +0200

    Incrementing the like counter

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 
 1. Initialized app with `create-react-app`
+2. Added a `LikeCounter` components that increments
--- src/components/LikeCounter.tsx
+++ src/components/LikeCounter.tsx
@@ -1,16 +1,20 @@
 // src/components/LikeCounter.tsx
-import React, { useState } from "react"; // <- note the added import of useState
+import React, { useState } from "react";
 
 export default function LikeCounter() {
   const initial_numLikes = 0;
   const [numLikes, set_numLikes] = useState(initial_numLikes); // <- using state!
 
-  console.log("what are these?", numLikes, set_numLikes);
+  const increment = () => {
+    console.log("Yes, clicked! Current number of likes:", numLikes);
+    set_numLikes(numLikes + 1);
+  };
 
   return (
     <div>
       <p>
         This post has <b>{numLikes}</b> likes!
+        <button onClick={increment}>Like</button>
       </p>
     </div>
   );
